8e7aaf6795c9c486a97e5027721558e6bfcd8679
Manasisaner <snehasonwane01@gmail.com>
2022-08-02 21:42:43 +0530

    [Manasi] Trash notes and Archive notes data rendering

--- Js/dashbord.js
+++ Js/dashbord.js
@@ -10,18 +10,7 @@ function openNav() {
 function togglePopup(d) {
   console.log(d)
   $(".popup").toggle();
- 
-
 }
-
-// function OpenModal() {
-//   let element = document.getElementById('div2')
-//   element.style.display = 'block'
-// }
-// function CloseModal() {
-//   let element = document.getElementById('div2')
-//   element.style.display = 'none'
-// }
 
 function replace() {
   document.getElementById("div1").style.display = "none";
@@ -54,8 +43,8 @@ btn.addEventListener('click', () => {
   });
 });
 
-  const myBtn = document.getElementById('myBtn');
-  myBtn.addEventListener('click', function handleClick(event) {
+const myBtn = document.getElementById('myBtn');
+myBtn.addEventListener('click', function handleClick(event) {
   document.getElementById("div2").style.display = "none";
   document.getElementById("div1").style.display = "block";
 });
@@ -68,32 +57,26 @@ window.onload = function () {
 };
 
 var notearray;
+let token = localStorage.getItem('token');
 getNotes();
 // getdata();
-   
+
 //GetNotes List Api code
-function getNotes(){
-let token = localStorage.getItem('token');
-$.ajax({
-  url: 'http://fundoonotes.incubation.bridgelabz.com/api/notes/getNotesList',
-  type: 'GET',
-  'Content-Type': 'application/json',
-  headers: { 'Authorization': token },
-  success: function (result) {
-    console.log(result.data.data);
-     notearray = result.data.data;
-     note = notearray.filter((element)=>{
-      return element.isDeleted === false && element.isArchived===false;
+function getNotes() {
+  
+  $.ajax({
+    url: 'http://fundoonotes.incubation.bridgelabz.com/api/notes/getNotesList',
+    type: 'GET',
+    'Content-Type': 'application/json',
+    headers: { 'Authorization': token },
+    success: function (result) {
+      console.log(result.data.data);
+      notearray = result.data.data;
+      note = notearray.filter((element) => {
+        return element.isDeleted === false && element.isArchived === false;
+      })
 
-    })
-
-    // let archivearray = result.data.data;
-    // let arch = archivearray.filter((element)=>{
-    //   return element.isArchived=== true;
-
-    // })
-
-    document.getElementById('demo').innerHTML = note.map((e) =>`
+      document.getElementById('demo').innerHTML = note.map((e) => `
     <div class ="box">
            <div onclick="togglePopup()" title=${e.title} description=${e.description}>
             <p >${e.title}</p>
@@ -137,140 +120,205 @@ $.ajax({
                   </div>
               </div>
       </div>`
-    )
-  },
-  error: function (error) {
-    console.log(error);
+      )
+    },
+    error: function (error) {
+      console.log(error);
 
-  }
-});
-
-
-// document.getElementById("togglebtn").addEventListener("click",function()
-// {
-//   var notes =document.getElementById("trash");
-//   if(notes.style.display == "none")
-//   {
-//     notes.style.display ="block";
-//   }
-//   else
-//   {
-//     notes.style.display = "none";
-//   }
-// }
-// )
-
-
-// function myFunction() {
-//   console.log("hii")
-//   var x = document.getElementById("trashnotes");
-//   if (x.style.display === "none") {
-//     x.style.display = "block";
-//   } 
-//   else {
-//     x.style.display = "none";
-//   }
-// }
-
-// function remove() {
-//   var x = document.getElementById("selectNow");
-//   x.remove(x.this);
-// }
-
-//  const deletenote = document.querySelector('#demo')
-//  deletenote.addEventListener('click',(e)=>{
-//   console.log(e.target.id);
-//   // var Card = document.getElementById('id'); 
-//   let req = {
-//   noteIdList: [e.target.id],
-//   isDeleted: true,
-// }
-//  console.log(token)
-// $.ajax({
-//     url: 'http://fundoonotes.incubation.bridgelabz.com/api/notes/trashNotes',
-//     type: 'POST',
-//     data:req,
-//     'Content-Type': 'application/json',
-//     headers: { 'Authorization': token },
-//     success: function (result) {
-//         console.log(result);
-//     },
-//     error: function (error) {
-//       console.log(error);
-//     }
-//  });
-// })
-
-
-// Delete note Api call
-
-
-
-const deletenote = document.querySelector('#demo')
-deletenote.addEventListener('click',(e)=>{
-let req= {
-  "noteIdList":[e.target.id],
-  "isDeleted": true
-  }
-  $.ajax({
-  url: 'http://fundoonotes.incubation.bridgelabz.com/api/notes/trashNotes',
-  type: 'POST',
-  data:JSON.stringify(req),
-  headers: {
-  'Content-Type': 'application/json',
-  'Authorization': token,
-  },
-  success: function (result) {
-  console.log(result);
-  
-  },
-  error: function (error) {
-  console.log(error);
-  }
+    }
   });
-})
-
 }
 
+  // Delete note Api call
 
-// function getdata()
-// {
-//   note = notearray.filter((element)=>{
-//     return element.isDeleted === false && element.isArchived===false;
-//   })
-//   displayNotes(note)
-// }
+  const deletenote = document.querySelector('#demo')
+  deletenote.addEventListener('click', (e) => {
+    console.log(trash)
+    let req = {
+      "noteIdList": [e.target.id],
+      "isDeleted": true
+    }
+    $.ajax({
+      url: 'http://fundoonotes.incubation.bridgelabz.com/api/notes/trashNotes',
+      type: 'POST',
+      data: JSON.stringify(req),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': token,
+      },
+      success: function (result) {
+        console.log(result);
 
-document.getElementById('nots').addEventListener('click',(c)=>{
-  console.log("get note",c.target);
-  note = notearray.filter((element)=>{
-    return element.isDeleted === false && element.isArchived===false;
+      },
+      error: function (error) {
+        console.log(error);
+      }
+    });
+  })
+
+  const color = ["#fff", "#f28b82", "#fbbc04", "#fff475", "#ccff90", "#a7ffeb", "#cbf0f8", "#aecbfa", "#d7aefb", "#fdcfe8", "#e6c9a8", "#e8eaed"]
+
+  $(document).on('click', '.30', function (e) {
+    updateColor = e.currentTarget.id
+    console.log(e.currentTarget.id)
+    console.log(updateColor)
+    console.log("hii")
+    let req = {
+      color: updateColor,
+      noteIdList: [e.target.id],
+    }
+
+    $.ajax({
+      url: 'http://fundoonotes.incubation.bridgelabz.com/api/notes/changesColorNotes',
+      type: 'POST',
+      data: JSON.stringify(req),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': token,
+      },
+      success: function (result) {
+        console.log(result);
+
+      },
+      error: function (error) {
+        console.log(error);
+      }
+    });
+  })
+
+
+
+
+
+document.getElementById('nots').addEventListener('click', (c) => {
+  console.log("get note", c.target);
+  note = notearray.filter((element) => {
+    return element.isDeleted === false && element.isArchived === false;
   })
   displayNotes(note)
 })
-document.getElementById('archive').addEventListener('click',(c)=>{
-  console.log("archive note",c.target);
-  archivearray = notearray.filter((element)=>{
-    return element.isArchived===true;
+document.getElementById('archive').addEventListener('click', (c) => {
+  console.log("archive note", c.target);
+  archivearray = notearray.filter((element) => {
+    return element.isArchived === true;
   })
-  displayNotes(archivearray)
+  displayArchive(archivearray)
 })
-document.getElementById('trash').addEventListener('click',(c)=>{
-  console.log("trash note",c.target);
-  trasharray = notearray.filter((element)=>{
-    return element.isDeleted===true;
+document.getElementById('trash').addEventListener('click', (c) => {
+  console.log("trash note", c.target);
+  trasharray = notearray.filter((element) => {
+    return element.isDeleted === true;
   })
-  displayNotes(trasharray)
+  displaytrash(trasharray)
 })
 
-function displayNotes(allnt){
-  let data =allnt
-  document.getElementById('demo').innerHTML= data.map((b)=>
-  `<div>
+function displayNotes(allnt) {
+  let data = allnt
+  document.getElementById('demo').innerHTML = data.map((b) =>
+    `<div class="box" style="border:1px solid green;">
   <p>${b.title}</p>
   <p>${b.isDeleted}</p>
   <p>${b.isArchived}</p>
-  <button id ="${b.id}" title="${b.title}">next</button>
+  <div class="newbox">
+  <img src="../assets/bell.svg" style=" height: 18px; width: 18px;  margin-right: 15px;" >
+  <img src="../assets/user.svg" style=" height: 18px; width: 18px;  margin-right: 15px;">
+  <div class="dropdown">
+  <span class="dropbtn"><img src="../assets/cpallet.svg"  style=" height: 18px; width: 18px;">
+ </span>
+ <div class = "dropdown-content1" style="left:0;">
+ <div class = "color">
+ <a href="#"  id="#a7ffeb" style="background-color:#a7ffeb"> </a>
+ <a href="#"  id="cbf0f8" style="background-color:#cbf0f8"> </a>
+ <a href="#"   id="#aecbfa" style="background-color:#aecbfa"> </a>
+ <a href="#"  id="#d7aefb" style="background-color:#d7aefb"> </a>
+ <a href="#"   id="#fdcfe8" style="background-color:#fdcfe8"> </a>
+ <a href="#"  id="#e6c9a8" style="background-color:#e6c9a8"> </a>
+ <a href="#"  id="#e8eae" style="background-color:#e8eae"> </a>
+ <a href="#"  id="#fff475" style="background-color:#fff475"> </a>
+ <a href="#"  id="#ccff90" style="background-color:#ccff90"> </a>
+ <a href="#"  id="#fbbc04" style="background-color:#fbbc04"> </a>
+ <a href="#" id="#f28b82"" style="background-color:#f28b82"> </a>
+ <a href="#"  id="#a7ffeb" style="background-color:#a7ffeb"> </a>
+</div>
+</div>
+ </div>
+<img src="../assets/gallery.svg"  style=" height: 18px; width: 18px;  margin-right: 15px;" >
+<a href="#" id=${b.id} ><img src="../assets/download.svg"  style=" height: 18px; width: 18px;  margin-right: 15px;"></a>
+<div class="dropdown">
+      <span class="dropbtn"><img src="../assets/3dot.svg" height: 10px; width: 10px;></span>
+      <div class="dropdown-content" style="left:0;">
+        <a href="#"  id=${b.id}>Delete note</a>
+        <a href="# ">Add label</a>
+        <a href="#">Add drawing</a>
+        <a href="#">Make a copy</a>
+        <a href="#">Show checkboxes</a>
+        <a href="#">Copy to Google Docs</a>
+      </div>
+</div>
+</div>
   </div>`
+  )
+}
+
+function displayArchive(allnt) {
+  let data = allnt
+  document.getElementById('demo').innerHTML = data.map((b) =>
+    `<div class="box" style="border:1px solid green;">
+  <p>${b.title}</p>
+  <p>${b.isDeleted}</p>
+  <p>${b.isArchived}</p>
+  <div class="newbox">
+  <img src="../assets/bell.svg" style=" height: 18px; width: 18px;  margin-right: 15px;" >
+  <img src="../assets/user.svg" style=" height: 18px; width: 18px;  margin-right: 15px;">
+  <div class="dropdown">
+  <span class="dropbtn"><img src="../assets/cpallet.svg"  style=" height: 18px; width: 18px;">
+ </span>
+ <div class = "dropdown-content1" style="left:0;">
+ <div class = "color">
+ <a href="#"  id="#a7ffeb" style="background-color:#a7ffeb"> </a>
+ <a href="#"  id="cbf0f8" style="background-color:#cbf0f8"> </a>
+ <a href="#"   id="#aecbfa" style="background-color:#aecbfa"> </a>
+ <a href="#"  id="#d7aefb" style="background-color:#d7aefb"> </a>
+ <a href="#"   id="#fdcfe8" style="background-color:#fdcfe8"> </a>
+ <a href="#"  id="#e6c9a8" style="background-color:#e6c9a8"> </a>
+ <a href="#"  id="#e8eae" style="background-color:#e8eae"> </a>
+ <a href="#"  id="#fff475" style="background-color:#fff475"> </a>
+ <a href="#"  id="#ccff90" style="background-color:#ccff90"> </a>
+ <a href="#"  id="#fbbc04" style="background-color:#fbbc04"> </a>
+ <a href="#" id="#f28b82"" style="background-color:#f28b82"> </a>
+ <a href="#"  id="#a7ffeb" style="background-color:#a7ffeb"> </a>
+</div>
+</div>
+ </div>
+<img src="../assets/gallery.svg"  style=" height: 18px; width: 18px;  margin-right: 15px;" >
+<a href="#" id=${b.id} ><img src="../assets/unarchive.svg"  style=" height: 18px; width: 18px;  margin-right: 15px;"></a>
+<div class="dropdown">
+      <span class="dropbtn"><img src="../assets/3dot.svg" height: 10px; width: 10px;></span>
+      <div class="dropdown-content" style="left:0;">
+        <a href="#"  id=${b.id}>Delete note</a>
+        <a href="# ">Add label</a>
+        <a href="#">Add drawing</a>
+        <a href="#">Make a copy</a>
+        <a href="#">Show checkboxes</a>
+        <a href="#">Copy to Google Docs</a>
+      </div>
+</div>
+</div>
+  </div>`
+  )
+}
+
+function displaytrash(allnt) {
+  let data = allnt
+  document.getElementById('demo').innerHTML = data.map((b) =>
+    `<div class="box" style="border:1px solid green;">
+  <p>${b.title}</p>
+  <p>${b.isDeleted}</p>
+  <p>${b.isArchived}</p>
+  <div class="newbox">
+  <img src="../assets/delete1.svg" style=" height: 18px; width: 18px;  margin-right: 15px;" >
+  <img src="../assets/delete.svg" style=" height: 18px; width: 18px;  margin-right: 15px;"> 
+</div>
+</div>`
   )
 }
