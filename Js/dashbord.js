b83a3cebb404928c8952e2085cfa407fb621747b
Manasisaner <snehasonwane01@gmail.com>
2022-08-04 21:54:32 +0530

    [Manasi] update notes

--- Js/dashbord.js
+++ Js/dashbord.js
@@ -7,10 +7,7 @@ function openNav() {
     nav.style.width = '250px';
   }
 }
-function togglePopup(d) {
-  console.log(d)
-  $(".popup").toggle();
-}
+
 
 function replace() {
   document.getElementById("div1").style.display = "none";
@@ -42,6 +39,41 @@ btn.addEventListener('click', () => {
     }
   });
 });
+// function editProduct(){
+
+//   document.getElementById("${e.title}").value = productID;
+//   document.getElementById("${e.description}").value = product_desc;
+
+
+// const mcloseButton = document.getElementById('UpBtn')
+
+// mcloseButton.addEventListener('click', () =>{
+//   console.log('clickedddd')
+//   modal.style.display = "none"
+//  // document.querySelector(".modal-container").style.display = "none"
+
+//   let data = {
+//     noteId: updateId,
+//     title: title.value,
+//     description: desc.value,
+//   }
+//   console.log(data);
+//   let token = localStorage.getItem('token')
+//   $.ajax({
+//     url: 'http://fundoonotes.incubation.bridgelabz.com/api/notes/updateNotes',
+//     type: 'POST',
+//     data: data,
+//     'Content-Type': 'application/json',
+//     headers: { 'Authorization': token },
+//     success: function (result) {
+//       console.log(result);
+//     },
+//     error: function (error) {
+//       console.log(error);
+//     }
+//   });
+// });
+// }
 
 const myBtn = document.getElementById('myBtn');
 myBtn.addEventListener('click', function handleClick(event) {
@@ -63,7 +95,7 @@ getNotes();
 
 //GetNotes List Api code
 function getNotes() {
-  
+
   $.ajax({
     url: 'http://fundoonotes.incubation.bridgelabz.com/api/notes/getNotesList',
     type: 'GET',
@@ -78,15 +110,15 @@ function getNotes() {
 
       document.getElementById('demo').innerHTML = note.map((e) => `
     <div class ="box" style="background-color:${e.color}" >
-           <div onclick="togglePopup()" title=${e.title} description=${e.description}>
-            <p >${e.title}</p>
+           <div id="UpBtn" title=${e.title} description=${e.description}>
+            <p  title=${e.title} id=${e.description} >${e.title} </p>
             <p>${e.description}</p>
             </div>
-              <div class="box2">
-                    <img src="../assets/bell.svg" style=" height: 18px; width: 18px;  margin-right: 15px;" >
-                    <img src="../assets/user.svg" style=" height: 18px; width: 18px;  margin-right: 15px;">
+              <div class="box2 d-flex">
+                    <img src="../assets/bell.svg" style=" width: 18px;  margin-right: 15px;" >
+                    <img src="../assets/user.svg" style=" width: 18px;  margin-right: 15px;">
                  <div class="dropdown">
-                    <span class="dropbtn"><img src="../assets/cpallet.svg"  style=" height: 18px; width: 18px;">
+                    <span class="dropbtn"><img src="../assets/cpallet.svg"  style=" width: 18px;">
                    </span>
                    <div class = "dropdown-content1" style="left:0;">
                    <div class = "color">
@@ -105,12 +137,12 @@ function getNotes() {
                  </div>
                 </div>
                </div>
-                    <img src="../assets/gallery.svg"  style=" height: 18px; width: 18px;  margin-right: 15px;" >
+                    <img src="../assets/gallery.svg"  style=" width: 18px;  margin-right: 15px;" >
                     <div id="Archive">
-                  <a href="#"><img id=${e.id} src="../assets/download.svg" ></a>
+                  <a href="#"><img id=${e.id} src="../assets/download.svg"  style=" width: 18px;  margin-right: 15px;"></a>
                  </div>
                   <div class="dropdown">
-                        <span class="dropbtn"><img src="../assets/3dot.svg" height: 10px; width: 10px;></span>
+                        <span class="dropbtn"><img src="../assets/3dot.svg"  width: 10px;></span>
                         <div class="dropdown-content" style="left:0;">
                           <div id="Itrash">
                           <a href="#" id=${e.id}>Delete</a>
@@ -133,50 +165,23 @@ function getNotes() {
   });
 }
 
-  // Delete note Api call
+// Delete note Api call
 
-  // const deletenote = document.querySelector('#demo')
-  // deletenote.addEventListener('click', (e) => {
-  //   console.log(trash)
-  //   let req = {
-  //     "noteIdList": [e.target.id],
-  //     "isDeleted": true
-  //   }
+// const deletenote = document.querySelector('#demo')
+// deletenote.addEventListener('click', (e) => {
+//   console.log(trash)
+//   let req = {
+//     "noteIdList": [e.target.id],
+//     "isDeleted": true
+//   }
 
-  $(document).on('click', '#Itrash', function (event) {
-    console.log('deletetrue')
-    console.log(event.target.id)
-    let req =
-    {
-        "noteIdList": [event.target.id],
-        "isDeleted": true
-    }
-    $.ajax({
-      url: 'http://fundoonotes.incubation.bridgelabz.com/api/notes/trashNotes',
-      type: 'POST',
-      data: JSON.stringify(req),
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': token,
-      },
-      success: function (result) {
-        console.log(result);
-
-      },
-      error: function (error) {
-        console.log(error);
-      }
-    });
-  })
-
-//restore note Api Call
-$(document).on('click', '#Restore', function (event) {
-  console.log('deletefalse')
+$(document).on('click', '#Itrash', function (event) {
+  console.log('deletetrue')
   console.log(event.target.id)
   let req =
   {
-      "noteIdList": [event.target.id],
-      "isDeleted": false
+    "noteIdList": [event.target.id],
+    "isDeleted": true
   }
   $.ajax({
     url: 'http://fundoonotes.incubation.bridgelabz.com/api/notes/trashNotes',
@@ -196,88 +201,142 @@ $(document).on('click', '#Restore', function (event) {
   });
 })
 
+//restore note Api Call
+$(document).on('click', '#Restore', function (event) {
+  console.log('deletefalse')
+  console.log(event.target)
+  let req =
+  {
+    "noteIdList": [event.target.title],
+    "isDeleted": false
+  }
+  $.ajax({
+    url: 'http://fundoonotes.incubation.bridgelabz.com/api/notes/trashNotes',
+    type: 'POST',
+    data: JSON.stringify(req),
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': token,
+    },
+    success: function (result) {
+      console.log(result);
+
+    },
+    error: function (error) {
+      console.log(error);
+    }
+  });
+})
+
+//Delete Forever Api Call
+$(document).on('click', '#Delete', function (a) {
+  console.log('deletetrue')
+  console.log(a.target)
+  let req = {
+    noteIdList: [a.target.title],
+    isDeleted: false,
+  }
+
+  $.ajax({
+    url: 'http://fundoonotes.incubation.bridgelabz.com/api/notes/deleteForeverNotes',
+    type: 'POST',
+    data: JSON.stringify(req),
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': token,
+    },
+    success: function (result) {
+      console.log(result);
+
+    },
+    error: function (error) {
+      console.log(error);
+    }
+  });
+})
+
 // Archived note Api call
-   $(document).on('click', '#Archive', function (a) {
-    console.log('Archivetrue')
-    console.log(a.target)
-    let req = {
-      "noteIdList": [a.target.id],
-      "isArchived": true,
+$(document).on('click', '#Archive', function (a) {
+  console.log('Archivetrue')
+  console.log(a.target)
+  let req = {
+    "noteIdList": [a.target.id],
+    "isArchived": true,
+  }
+  $.ajax({
+    url: 'http://fundoonotes.incubation.bridgelabz.com/api/notes/archiveNotes',
+    type: 'POST',
+    data: JSON.stringify(req),
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': token,
+    },
+    success: function (result) {
+      console.log(result);
+
+    },
+    error: function (error) {
+      console.log(error);
     }
-    $.ajax({
-      url: 'http://fundoonotes.incubation.bridgelabz.com/api/notes/archiveNotes',
-      type: 'POST',
-      data: JSON.stringify(req),
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': token,
-      },
-      success: function (result) {
-        console.log(result);
-
-      },
-      error: function (error) {
-        console.log(error);
-      }
-    });
-  })
+  });
+})
 
 
-  // UnArchived note Api call
-  $(document).on('click', '#unarchive', function (a) {
-    console.log('Archivefalse')
-    console.log(a.target)
-    let req = {
-      "noteIdList": [a.target.id],
-      "isArchived": false,
+// UnArchived note Api call
+$(document).on('click', '#unarchive', function (a) {
+  console.log('Archivefalse')
+  console.log(a.target)
+  let req = {
+    "noteIdList": [a.target.id],
+    "isArchived": false,
+  }
+  $.ajax({
+    url: 'http://fundoonotes.incubation.bridgelabz.com/api/notes/archiveNotes',
+    type: 'POST',
+    data: JSON.stringify(req),
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': token,
+    },
+    success: function (result) {
+      console.log(result);
+
+    },
+    error: function (error) {
+      console.log(error);
     }
-    $.ajax({
-      url: 'http://fundoonotes.incubation.bridgelabz.com/api/notes/archiveNotes',
-      type: 'POST',
-      data: JSON.stringify(req),
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': token,
-      },
-      success: function (result) {
-        console.log(result);
+  });
+})
+//const color = ["#fff", "#f28b82", "#fbbc04", "#fff475", "#ccff90", "#a7ffeb", "#cbf0f8", "#aecbfa", "#d7aefb", "#fdcfe8", "#e6c9a8", "#e8eaed"]
 
-      },
-      error: function (error) {
-        console.log(error);
-      }
-    });
-  })
-  //const color = ["#fff", "#f28b82", "#fbbc04", "#fff475", "#ccff90", "#a7ffeb", "#cbf0f8", "#aecbfa", "#d7aefb", "#fdcfe8", "#e6c9a8", "#e8eaed"]
+$(document).on('click', '.color', function (n) {
+  updateColor = n.target.id
+  console.log(n.target)
+  console.log(updateColor)
+  console.log("hii")
+  var noteid = n.target.noteid
+  let req = {
+    "color": updateColor,
+    "noteIdList": [n.target.title],
+  }
 
-  $(document).on('click', '.color', function (n) {
-    updateColor = n.target.id
-    console.log(n.target)
-    console.log(updateColor)
-    console.log("hii")
-    var noteid=n.target.noteid
-    let req = {
-      "color": updateColor,
-      "noteIdList": [n.target.title],
+  $.ajax({
+    url: 'http://fundoonotes.incubation.bridgelabz.com/api/notes/changesColorNotes',
+    type: 'POST',
+    data: JSON.stringify(req),
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': token,
+    },
+    success: function (result) {
+      console.log(result);
+
+    },
+    error: function (error) {
+      console.log(error);
     }
-
-    $.ajax({
-      url: 'http://fundoonotes.incubation.bridgelabz.com/api/notes/changesColorNotes',
-      type: 'POST',
-      data: JSON.stringify(req),
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': token,
-      },
-      success: function (result) {
-        console.log(result);
-
-      },
-      error: function (error) {
-        console.log(error);
-      }
-    });
-  })
+  });
+})
 
 
 
@@ -409,11 +468,40 @@ function displaytrash(allnt) {
   <p>${b.title}</p>
   <p>${b.description}</p>
   <div class="newbox">
-  <img src="../assets/delete1.svg" style=" height: 18px; width: 18px;  margin-right: 15px;" >
-  <img id="Restore" src="../assets/delete.svg" style=" height: 18px; width: 18px;  margin-right: 15px;"> 
+  <img id="Delete" title=${b.id} src="../assets/delete1.svg" style=" height: 18px; width: 18px;  margin-right: 15px;" >
+  <img id="Restore" title=${b.id} src="../assets/delete.svg" style=" height: 18px; width: 18px;  margin-right: 15px;"> 
 </div>
 </div>`
   )
 }
 
- 
+document.getElementById('demo').addEventListener('click', (d) => {
+  console.log(d.target.id,d.target.title)
+  //$(".popup").toggle();
+  document.querySelector('.overlay').innerHTML =
+  ` <div id="div2"  class="col-md-12" >
+  <div>
+  <div class=" mt-3 d-flex popup">
+    <input type="text" class="mb-2" id="ttl" placeholder=${d.target.title} autocomplete="off">
+    <img src="../assets/pin.svg">
+    </div>
+  <input type="text" class="mb-2 mt-3" id="desc" placeholder=${d.target.id} autocomplete="off">
+  
+  <div class="d-flex  justify-content-between ">
+    <div class="col-md-10">
+      <img src="../assets/bell.svg">
+      <img src="../assets/user.svg">
+      <img src="../assets/cpallet.svg">
+      <img src="../assets/gallery.svg">
+      <img src="../assets/download.svg">
+      <img src="../assets/3dot.svg" style=" height: 18px; width: 18px;">
+      <img src="../assets/back.svg">
+      <img class="redu" src="../assets/back.svg">
+    </div>
+    <div class="col-md-2">
+      <button type="submit" id="myBtn" value="submit" class="btn btn-light mb-2">Close</button>
+    </div>
+    </div>
+  </div>
+</div>`
+})
